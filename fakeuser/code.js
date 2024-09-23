0acb1c5b0606212b3ffb4b0c9b025bed764e8ae8
Deltta <gustavodeltta@gmail.com>
2024-09-23 11:08:34 -0300

    Aula 23.09 - Finalização dos filtros em JS e criação de uma breve documentação

--- fakeuser/code.js
+++ fakeuser/code.js
@@ -10,17 +10,40 @@ async function getUser(){
         gender = inputsRadio[2].value;
     }
 
-    //https://radomuser.me/api/?results=1&nat=br&gender=
+    var valueSelect = document.querySelector("select").value;
 
-    var result = await fetch("https://randomuser.me/api/?results=100&nat=br&gender=");
+    var result = await fetch(
+        "https://randomuser.me/api/?results=" + 
+        valueSelect + 
+        "&nat=br&gender=" + 
+        gender
+    );
 
     var infos = await result.json();
 
     var allUsers = infos.results;
     var name;
 
+    var allFakeUsers = document.querySelector(".allFakeUsers")
+    allFakeUsers.innerHTML = "";
+
     for (i = 0; i < allUsers.length; i++){
         name = allUsers[i].name;
         console.log(name.first + " " + name.last);
+
+        var fakeUser = document.createElement("div");
+        fakeUser.classList.add("FakeUsers");
+
+        fakeUser.innerHTML = `
+            <div class="image">
+                <img class="img" src="${allUsers[i].picture.large}"/> 
+            </div>
+            <p><b>Nome: </b> <span>${name.first + " " + name.last}</span></p>
+            <p><b>Email: </b> <span>${allUsers[i].email}</span></p>
+            <p><b>Idade: </b> <span>${allUsers[i].dob.age}</span></p>
+            <p><b>Cidade: </b> <span>${allUsers[i].location.city}</span></p>
+        `;
+        allFakeUsers.appendChild(fakeUser)
+
     }
 }
